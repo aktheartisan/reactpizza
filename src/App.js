e3e598d91a60ecf263d64862efe5e89a024e8bdb
aktheartisan <aravinthkumaran410@gmail.com>
2024-01-02 23:09:45 +0530

    use fragment

--- src/App.js
+++ src/App.js
@@ -22,22 +22,43 @@ function Header() {
 }
 
 function Menu() {
+   const pizza = pizzaData;
+  //  const pizza = [];
+  const pizzaLength = pizza.length;
+  //use fragmet to place two element in a one jsx
   return (
     <div>
       <main className="menu">
         <h2>Menu</h2>
 
-        {pizzaData.map((pizza, index) => (
+        {pizzaLength>0 ? 
+        <>
+            
+        <p>hai hello welcome to ak pizza hut.we have six varity of pizza</p>
+        <ul className="pizzas"> 
+
+        {
+           pizza.map((pizza, index) => (
           <Pizza pizzaObj={pizza} key={index} />
-        ))}
+           )) 
+        }
+        </ul>
+        
+        </>
+      
+        : <p>sorry</p>}
       </main>
     </div>
   );
 }
 
+
 function Pizza(props) {
+
   let soldOut = props.pizzaObj.soldOut;
   console.log(props);
+
+  //using ternary to show if pizza variety is sold
 
   return (
     <div className="pizza">
@@ -59,23 +80,35 @@ function Footer() {
   let hour = new Date().getHours();
   // console.log(hour);
   let openHour = 15;
-  let closeHour = 22;
+  let closeHour = 24;
   let open = hour >= openHour && hour <= closeHour;
   console.log(open);
   return (
     <div>
       <footer>
         {open ? (
-          <div className="order">
-            <p>we are open</p>
-            <button className="btn">book now</button>
-          </div>
+          //send js as props to the openhour component
+          <Openhour open={openHour} close={closeHour}/>
         ) : (
           <p>we are closed</p>
         )}
       </footer>
     </div>
   );
+}
+
+//change jsx into components-openhour
+
+function Openhour(props){
+
+  //receive js as props to the openhour component
+
+  return (
+    <div className="order">
+            <p>we are open from {props.open}pm to {props.close}pm</p>
+            <button className="btn">book now</button>
+    </div>
+  )
 }
 
 export default App;
