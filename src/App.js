7e29c2c94272f3b6be224c9012f0d6843fb2c9de
Ak <akofficemail.com>
2024-01-02 18:22:50 +0530

    ternary operator used in pizza component

--- src/App.js
+++ src/App.js
@@ -1,80 +1,81 @@
-
-import './App.css';
-import pizzaData from './data';
+import "./App.css";
+import pizzaData from "./data";
 
 function App() {
   return (
-
-    <div className='container'> 
-      <Header/>
-      <Menu/>
-      <Footer/>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
-  
   );
 }
 
-function Header(){
-
-  return(
-
-    <div>
-      <header><h2>Ak pizza co</h2></header>
-    </div>
-  )
-}
-
-function Menu(){
-
+function Header() {
   return (
-
     <div>
-      <main className='menu'> 
-         <h2>Menu</h2>
-
-         {pizzaData.map((pizza)=>(
-          
-          <Pizza name={pizza.name} photo={pizza.photoName} ingredient={pizza.ingredients} price={pizza.price}/>
-
-         ))}
-       
- 
-       </main>
-    
+      <header>
+        <h2>Ak pizza co</h2>
+      </header>
     </div>
-  )
+  );
 }
 
-function Pizza(props){
+function Menu() {
+  return (
+    <div>
+      <main className="menu">
+        <h2>Menu</h2>
 
+        {pizzaData.map((pizza, index) => (
+          <Pizza pizzaObj={pizza} key={index} />
+        ))}
+      </main>
+    </div>
+  );
+}
+
+function Pizza(props) {
+  let soldOut = props.pizzaObj.soldOut;
   console.log(props);
 
-  return(
-
-    <div className='pizza'>
-      <img src={props.photo} alt=''/>
-      <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredient}</p>
-        <span>{props.price + 10}</span>
-      </div>
-      
+  return (
+    <div className="pizza">
+      <img src={props.pizzaObj.photoName} alt="" />
+      {soldOut ? (
+        <p>sorry it is finished</p>
+      ) : (
+        <div>
+          <h3>{props.pizzaObj.name}</h3>
+          <p>{props.pizzaObj.ingredients}</p>
+          <span>{props.pizzaObj.price + 10}</span>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-function Footer(){
-
-  return(
-
+function Footer() {
+  let hour = new Date().getHours();
+  // console.log(hour);
+  let openHour = 15;
+  let closeHour = 22;
+  let open = hour >= openHour && hour <= closeHour;
+  console.log(open);
+  return (
     <div>
-      <footer><h2>{new Date().toLocaleTimeString()} we are opened.</h2></footer>
+      <footer>
+        {open ? (
+          <div className="order">
+            <p>we are open</p>
+            <button className="btn">book now</button>
+          </div>
+        ) : (
+          <p>we are closed</p>
+        )}
+      </footer>
     </div>
-  )
+  );
 }
-
-
-
-
 
 export default App;
